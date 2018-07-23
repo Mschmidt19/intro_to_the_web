14932f32c72649fa4d7bc4976114383e8eb140e0
Marek <MarekSchmidt@outlook.com>
2018-07-23 16:34:26 +0100

    Params

--- lib/app.rb
+++ lib/app.rb
@@ -12,10 +12,17 @@ get '/tomato' do
   'Tomato cheese'
 end
 
-get '/cat' do
-  @random_name = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
 
 get '/marek' do
   erb (:marek)
